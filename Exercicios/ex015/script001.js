4d5bbbd5876e2e9ea5b253674d22a2414a3a332c
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-12 12:00:55 -0300

    ex015.2

---
+++ Exercicios/ex015/script001.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.sort() // Colocar em ordem
+num.push(1) // Adicionar 1 no ultimo slot
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`) // Número de elementos
+console.log(`O primeiro valor do vetor é ${num[0]}`) // Mostrar o valor de cada elemento um de cada vez
