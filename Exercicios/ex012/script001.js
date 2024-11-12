a6fb110988e073d708ced141597808443fef751f
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-12 09:26:23 -0300

    ex012

---
+++ Exercicios/ex012/script001.js
@@ -0,0 +1,19 @@
+/*
+var c = 1
+while (c <= 1500) {
+    console.log(`Olá, tudo bem?${c}`)
+    c++
+}
+*/
+
+/*
+var c = 1
+do {
+    console.log(`Olá, tudo bem?${c}`)
+    c++
+} while (c <= 1500)
+ */
+
+for (var c = 1; (c <= 1500); c++){
+    console.log(`Olá, tudo bem? ${c}`)
+}
